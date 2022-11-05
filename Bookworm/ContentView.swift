f27c446a57c3e2f32e9df33b09f3308612d6395e
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-05 12:35:20 +0100

    Fetching and creating data

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,15 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationSplitView {
+            ListingView()
+                .frame(minWidth: 250)
+        } detail: {
+            Text("Please select a review")
         }
-        .padding()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Bookworm/ListingView.swift
@@ -0,0 +1,46 @@
+//
+//  ListingView.swift
+//  Bookworm
+//
+//  Created by Sebastien REMY on 05/11/2022.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.id)]) var reviews: FetchedResults<Review>
+    
+    @AppStorage("id") var id = 1
+    
+    var body: some View {
+        List(reviews) { review in
+            Text(review.reviewTitle)
+        }
+        .toolbar{
+            Button(action: addReview) {
+                Label("Add review", systemImage: "plus")
+            }
+        }
+    }
+    
+    func addReview() {
+        let review = Review(context: managedObjectContext)
+        review.id = Int32(id)
+        review.title = "Enter the title"
+        review.author = "Enter the author"
+        review.rating = 3
+        
+        id += 1
+        
+        dataController.save()
+    }
+}
+
+struct ListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListingView()
+    }
+}
