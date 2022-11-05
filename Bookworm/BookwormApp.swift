7c1e506d06cf2f7d9dcea1ab6e43cf18ca1694ad
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-05 09:27:04 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sebastien REMY on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
