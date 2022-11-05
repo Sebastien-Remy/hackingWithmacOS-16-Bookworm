d81ab9b9054c6f3ec154e0dcf51ef69d7d7ee572
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-05 10:31:30 +0100

    Creating Data Model

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct BookwormApp: App {
+    
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(dataController)
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .onReceive(NotificationCenter.default.publisher(for: NSApplication.willTerminateNotification)) {_ in
+                    dataController.save()
+                }
         }
     }
 }
